1c3d8930d6ef639ad973a05b6532ab4bab0ca579
Hayley Schluter <Hydriad@users.noreply.github.com>
2020-11-30 15:40:01 -0700

    profile tab styling - not finished but better

--- web/src/routes/profile.svelte
+++ web/src/routes/profile.svelte
@@ -4,6 +4,12 @@
 
 	const tabs = ['watching', 'unwatched', 'completed'];
 	let tabOpen = 0;
+	const defaultShows = {
+			watching: [],
+			unwatched: [],
+			completed: []
+	};
+	let shows = { ...defaultShows	};
 
 	function changeOpenTab(tabIndex) {
 		tabOpen = tabIndex;
@@ -11,13 +17,7 @@
 
 	// given an array of shows, return an object of shows by category, split into 3 arrays
 	function sortShowsIntoCategories(shows) {
-		console.log({ shows} );
-
-		const categorizedShows = {
-			watching: [],
-			unwatched: [],
-			completed: []
-		};
+		const categorizedShows = { ...defaultShows };
 
 		for (let i = 0; i < shows.length; ++i) {
 			const show = shows[i];
@@ -28,12 +28,6 @@
 		return categorizedShows;
 	};
 
-	let shows = {
-			watching: [],
-			unwatched: [],
-			completed: []
-	};
-	
 	onMount(async () => {
 		shows = sortShowsIntoCategories(await getAllShows());
 	})
@@ -42,10 +36,9 @@
 <h1>My shows</h1>
 
 <!-- tabs -->
-<p>tab open: { tabOpen }</p>
-<div>
+<div class="tabs">
 	{ #each tabs as tab, i }
-		<a on:click={ () => changeOpenTab(i) }>{ tab }</a>
+		<a class="tab" on:click={ () => changeOpenTab(i) }>{ tab }</a>
 	{ /each }
 </div>
 
@@ -86,23 +79,35 @@
 		color: darkblue;
 	}
 
+	.tabs {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.tabs .tab {
+		padding: 0 .5rem;
+		text-transform: capitalize;
+		cursor: pointer;
+	}
+
 	.shows-list .show {
 		background-color: #eeeeee;
 		display: flex;
-		align-items: baseline;
+		align-items: center;
 		margin: .5rem 0;
 		padding: 0 .5rem;
 	}
 
 	.friends-watching-list .friend {
 		display: inline-block;
-		padding: .5rem;
-		margin: 0 .15rem;
+		font-size: .5rem;
+		padding: .2rem;
+		margin: 0 .1rem;
 		background-color: lightblue;
 		border: 2px orange solid;
-		width: 1rem;
-		height: 1rem;
-		border-radius: 3rem;
+		width: .5rem;
+		height: .5rem;
+		border-radius: 1rem;
 		text-align: center;
 	}
 
